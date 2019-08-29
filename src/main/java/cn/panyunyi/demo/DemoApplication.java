40958e95011db58a13e4f38406f9bedc025ee46c
panyunyi97 <779087031@qq.com>
2019-08-29 23:53:41 +0800

    init commit
    N/A
    
    init commit
    
    Signed-off-by: panyunyi97 <779087031@qq.com>

---
+++ src/main/java/cn/panyunyi/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package cn.panyunyi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class DemoApplication extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
